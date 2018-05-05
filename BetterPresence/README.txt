f1ff4372fc77551fddd3ef925f7ce8ab82412b12
Daniel Nesov <d.nesov@gmail.com>
2018-05-05 20:46:47 +0200

    Add files via upload

--- BetterPresence/README.txt
+++ BetterPresence/README.txt
@@ -1,4 +1,4 @@
-BetterPresence v0.1.0 coded by dnesov
+BetterPresence v0.2.0 coded by dnesov
 
 LICENSE:
 You can use the source code in your [whatever] even without crediting me. (But if you credit, i'll be happy :D)
--- BetterPresence/startpresence.bat
+++ BetterPresence/startpresence.bat
@@ -1,2 +1,18 @@
-node main.js
+@echo off
+title BetterPresence
+if exist "./node_modules" if exist "./node_modules/async-limiter" if exist "./node_modules/discord.js" if exist "./node_modules/discord-rpc" if exist "./node_modules/pako" if exist "./node_modules/prism-media" if exist "./node_modules/safe-buffer" if exist "./node_modules/snekfetch" if exist "./node_modules/tweetnacl" if exist "./node_modules/ws" goto startapp
+:dependenciesalert
+@echo In order to make this utility work, you need to install dependencies!
+@echo do you want to install them? (Y/N)
+set /p answer=
+if %answer% == Y goto installdependencies
+if %answer% == N goto exitapp
+
+:installdependencies
+@echo Installing dependencies...
+npm i discord-rpc
 pause
+:exitapp
+exit
+:startapp
+node ./src/main.js
